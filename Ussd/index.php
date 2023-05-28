d39cb3ce7f1d527bb90ebae85deb3cbef9ba6709
Kevin7744 <arapbiisubmissions@gmail.com>
2023-05-28 03:15:11 -0700

    Test 3

--- Ussd/index.php
+++ Ussd/index.php
@@ -5,31 +5,123 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
+// Function to create an account
+function createAccount()
+{
+    // Perform the necessary operations to create an account for the user
+    $accountNumber = generateAccountNumber(); // Generate a unique account number
+    $balance = 0; // Initialize account balance
+    
+    // Save the account details to the database or any other storage mechanism
+    saveAccountDetails($phoneNumber, $accountNumber, $balance);
+
+    // Return the response to be sent back
+    return "END Your account has been created successfully. You can now proceed to convert cash to beecoins.";
+}
+
+// Function to convert cash to beecoins
+function convertToBeecoins()
+{
+    $cashAmount = 100; // Placeholder value, replace with actual cash amount provided by the user
+
+    // Check if the user has enough cash to convert
+    if ($cashAmount >= 50) {
+        // Calculate the beecoins equivalent based on the conversion rate
+        $conversionRate = 2; // Placeholder value, replace with actual conversion rate
+        $beecoinsAmount = $cashAmount * $conversionRate;
+
+        // Perform the necessary operations to make the payment and update the user's account balance
+        $accountNumber = getAccountNumber($phoneNumber); // Retrieve the user's account number from the database or storage
+        $mainAddress = "2323"; // Placeholder value, replace with the actual main address
+        $transactionStatus = makePayment($accountNumber, $mainAddress, $beecoinsAmount);
+
+        if ($transactionStatus == "success") {
+            // If the payment is successful, update the user's account balance
+            updateAccountBalance($accountNumber, $beecoinsAmount);
+
+            // Return the response to be sent back
+            return "END Conversion and payment successful. Thank you for using our service!";
+        } else {
+            // If the payment fails, return an appropriate response
+            return "END Payment failed. Please try again later.";
+        }
+    } else {
+        // If the user doesn't have enough cash, return an appropriate response
+        return "END Insufficient cash to convert to beecoins.";
+    }
+}
+
+// Function to generate a unique account number
+function generateAccountNumber()
+{
+    // Generate a unique account number based on your requirements
+    // ...
+
+    // Placeholder implementation that generates a random 6-digit number
+    return "ACC" . rand(100000, 999999);
+}
+
+// Function to save the account details to the database or storage
+function saveAccountDetails($phoneNumber, $accountNumber, $balance)
+{
+    // Save the account details to the database or any other storage mechanism
+    // ...
+
+    // Placeholder implementation
+    // You would typically use database queries or an ORM to insert the data
+    // Here, we simply print the details for demonstration purposes
+    echo "Account Number: $accountNumber<br>";
+    echo "Phone Number: $phoneNumber<br>";
+    echo "Balance: $balance<br>";
+}
+
+// Function to retrieve the user's account number from the database or storage
+function getAccountNumber($phoneNumber)
+{
+    // Retrieve the user's account number from the database or any other storage mechanism
+    // ...
+
+    // Placeholder implementation that returns a hardcoded value for demonstration purposes
+    return "ACC1001";
+}
+
+// Function to make the payment and return the transaction status
+function makePayment($accountNumber, $mainAddress, $beecoinsAmount)
+{
+    // Perform the necessary operations to make the payment to the main address
+    // ...
+
+    // Placeholder implementation that always returns a successful transaction for demonstration purposes
+    return "success";
+}
+
+// Function to update the user's account balance
+function updateAccountBalance($accountNumber, $beecoinsAmount)
+{
+    // Perform the necessary operations to update the user's account balance in the database or storage
+    // ...
+
+    // Placeholder implementation
+    echo "Account balance updated. New balance: $beecoinsAmount beecoins<br>";
+}
+
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+    $response  = "CON What would you like to do? \n";
+    $response .= "1. Create an account \n";
+    $response .= "2. Convert cash to beecoins and make a payment";
 
 } else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
+    // Business logic for creating an account
+    $response = createAccount();
 
 } else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
-
-} else if($text == "1*1") { 
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
+    // Business logic for converting cash to beecoins and making a payment
+    $response = convertToBeecoins();
 
 }
 
 // Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
+?>
